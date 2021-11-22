b1ea4358c97593db4ab3532daae4c4bb07378951
Swastik Khuntia <swostikkhuntia@gmail.com>
2021-11-22 15:55:09 +0530

    Add files via upload

---
+++ esp32_final.ino
@@ -0,0 +1,164 @@
+#include "Arduino.h"
+#include "esp_camera.h"
+#include <WiFi.h>
+#include <base64.h>
+#include <HTTPClient.h>
+#include <SoftwareSerial.h> //For Software Serial
+
+// Select camera model
+//#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
+//#define CAMERA_MODEL_ESP_EYE // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
+//#define CAMERA_MODEL_M5STACK_WIDE  // Has PSRAM
+#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+//#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
+
+//#include "camera_pins.h"
+
+//Define software serial ports
+//Initialize softwareSerial
+//#define TX 12
+//#define RX 15
+//SoftwareSerial mySerial;
+//Define files used as a replacement to camer_pins.h libraray
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
+
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
+
+const char* ssid = "JioFiber-3G";
+const char* password = "khuntiafamily";
+
+void setup() {
+  Serial.begin(9600);
+  Serial.setDebugOutput(true);
+  Serial.println();
+
+  //Software serial begin
+  //mySerial.begin(9600,SWSERIAL_8N1,RX,TX);
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  
+  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
+  //                      for larger pre-allocated frame buffer.
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_QVGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_QVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+#if defined(CAMERA_MODEL_ESP_EYE)
+  pinMode(13, INPUT_PULLUP);
+  pinMode(14, INPUT_PULLUP);
+#endif
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+
+#if defined(CAMERA_MODEL_M5STACK_WIDE)
+  s->set_vflip(s, 1);
+  s->set_hmirror(s, 1);
+#endif
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  
+  classifyImage();
+ 
+}
+void classifyImage() {
+  
+  // Capture picture
+   camera_fb_t * fb = NULL;
+   fb = esp_camera_fb_get();
+   
+   if(!fb) {
+    Serial.println("Camera capture failed");
+    return;
+   }
+
+  size_t size = fb->len;
+  String buffer = base64::encode((uint8_t *) fb->buf, fb->len);
+  //String payload = "{\"inputs\": [{ \"data\": {\"image\": {\"base64\": \"" + buffer + "\"}}}]}";
+  String payload = "{\"input\": \""+ buffer +"\"}";
+
+  buffer = "";
+  // Uncomment this if you want to show the payload
+  Serial.println(payload);
+
+  esp_camera_fb_return(fb);
+  
+  // Generic model
+  //String model_id = "aaa03c23b3724a16a56b629203edc62c";
+
+  HTTPClient http;
+  //http.begin("https://api.clarifai.com/v2/models/" + model_id + "/outputs");
+  http.begin("http://dstri26.pythonanywhere.com/scan");
+  http.addHeader("Content-Type", "application/json");     
+  //http.addHeader("Authorization", "Key your_key"); 
+  int response_code = http.POST(payload);
+  Serial.print(response_code);
+  String response = http.getString();
+  Serial.println(response);
+}
+void loop()
+{
+  //Software Serial testing
+ // if(mySerial.available())
+  //{
+    //mySerial.write("Test");
+    Serial.write("123");
+    Serial.println("Communication established");
+    delay(3000);
+  //}
+ // Serial.print("Worked");
+  //delay(120000);
+}
