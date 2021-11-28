97f2ad4346b803db9f044637a322f64244590f6c
Trideep Barik <trideep.india@gmail.com>
2021-11-28 16:02:17 +0530

    Create app.py

---
+++ api/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request, jsonify, json
+import base64
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/scan', methods = ['GET','POST'])
+def scan():
+    if request.method == 'POST':
+        request_data = json.loads(request.data)
+        byte = request_data['input']
+        # decodeit = open('./static/img.jpeg', 'wb')
+        # decodeit.write(base64.b64decode((byte)))
+        # decodeit.close()
+        return jsonify({"code": "1"})
+
+    return jsonify({'flevel': "0"})
+
+if __name__ == '__main__':
+    app.run()
+
